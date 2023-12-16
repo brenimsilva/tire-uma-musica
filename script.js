704fbf81df610cfc26063dbbbcd59fb6d7e437ac
Breno Almeida <breno.almeida@nuria.com.br>
2023-12-16 16:40:58 -0300

    [Feature]: reset btn + level select

--- script.js
+++ script.js
@@ -4,64 +4,84 @@ const db_musicas = {
     3: ["Canção de Maio", "Long long ago", "Allegro", "Modo perpétuo", "azar"],
     4: ["Abelhinhas no jardim", "Margaridas", "Oh Suzana!", "azar"],
 }
-
+let level = 1;
 const titleLevel = document.getElementById("title-level");
-
-
-
 const appContainer = document.getElementById("app");
+const levelSelectElement = document.getElementById("level-select");
+const btnReset = document.getElementById("btn-reset");
+btnReset.addEventListener("click", (e) => {
+    render();
+})
+render()
+
+Object.entries(db_musicas).forEach(([key,value]) => {
+    const option = document.createElement("option");
+    option.text = `Level ${key}`
+    option.value = key
+    levelSelectElement.appendChild(option)
+})
+
+levelSelectElement.addEventListener("change", (e) => {
+        level = Number(e.target.value);
+        render();
+})
+
 const params = new URLSearchParams(window.location.search);
-const level = Number(params.get("level"));
+// const level = Number(params.get("level"));
 const quantidade_musicas = Number(params.get("qtd"));
-titleLevel.innerText = level.toString();
+function render() {
+    titleLevel.innerText = level.toString();
+    appContainer.innerHTML = ""
 
-const db_sorteado = []
+    const db_sorteado = []
+    
 
-while(db_sorteado.length < db_musicas[level].length) {
-    const randomIndex = Math.floor(Math.random() * db_musicas[level].length);
-    if(db_sorteado.includes(db_musicas[level][randomIndex])) continue;
-    db_sorteado.push(db_musicas[level][randomIndex]);
-}
+    while(db_sorteado.length < db_musicas[level].length) {
+        const randomIndex = Math.floor(Math.random() * db_musicas[level].length);
+        if(db_sorteado.includes(db_musicas[level][randomIndex])) continue;
+        db_sorteado.push(db_musicas[level][randomIndex]);
+    }
 
-let selected = false;
+    let selected = false;
 
-for(let i = 0; i < db_sorteado.length; i++) {
-    const musicContainer = document.createElement("div");
-    musicContainer.classList.add("music-container");
-    musicContainer.classList.add("card-back")
-    const p = document.createElement("p");
-    p.classList.add("music-text")
-    p.innerText = db_sorteado[i];
-    musicContainer.appendChild(p);
-    musicContainer.addEventListener("click", () => {
-        if(!selected) {
-            musicContainer.classList.add("card-turn")
-            if(p.innerText === "azar") {
-                musicContainer.addEventListener("animationend", () => {
-                    p.style.color = "black";
-                })
-                setTimeout(() => {
-                    const all = document.querySelectorAll(".music-container");
-                    all.forEach(t => {
-                        t.classList.add("card-turn")
-                        t.addEventListener("animationend", () => {
-                            t.style.color = "black"
-                        })
-                    });
-                }, 2000)
-            } else {
-                musicContainer.addEventListener("animationend", () => {
-                    p.style.color = "black";
-                })
+    for(let i = 0; i < db_sorteado.length; i++) {
+        const musicContainer = document.createElement("div");
+        musicContainer.classList.add("music-container");
+        musicContainer.classList.add("card-back")
+        const p = document.createElement("p");
+        p.classList.add("music-text")
+        p.innerText = db_sorteado[i];
+        musicContainer.appendChild(p);
+        musicContainer.addEventListener("click", () => {
+            if(!selected) {
+                musicContainer.classList.add("card-turn")
+                if(p.innerText === "azar") {
+                    musicContainer.addEventListener("animationend", () => {
+                        p.style.color = "black";
+                    })
+                    setTimeout(() => {
+                        const all = document.querySelectorAll(".music-container");
+                        all.forEach(t => {
+                            t.classList.add("card-turn")
+                            t.addEventListener("animationend", () => {
+                                t.style.color = "black"
+                            })
+                        });
+                    }, 2000)
+                } else {
+                    musicContainer.addEventListener("animationend", () => {
+                        p.style.color = "black";
+                    })
+                }
+                selected = true;
             }
-            selected = true;
-        }
-    })
+        })
 
-    // musicContainer.addEventListener("animationend", () => {
-    //     console.log("ENDED")
-    // })
-    appContainer.appendChild(musicContainer);
+        // musicContainer.addEventListener("animationend", () => {
+        //     console.log("ENDED")
+        // })
+        appContainer.appendChild(musicContainer);
+    }
 }
 
 
